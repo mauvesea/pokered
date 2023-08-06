e3d5fda74e7c9c7d6443c4fbd4b24baa8e7f4e30
SeasickShore <79065840+SeasickShore@users.noreply.github.com>
2023-08-06 01:47:44 -0300

    Trainers + Wild Encounters
    Trainers have been updated up to the Rocker class as well as all gym leaders/E4s. Some wild encounters were also updated.

--- data/wild/maps/PokemonMansion1F.asm
+++ data/wild/maps/PokemonMansion1F.asm
@@ -1,29 +1,15 @@
 PokemonMansion1FWildMons:
 	def_grass_wildmons 10 ; encounter rate
-IF DEF(_RED)
 	db 32, KOFFING
-	db 30, KOFFING
+	db 32, GRIMER
 	db 34, PONYTA
-	db 30, PONYTA
+	db 34, VULPIX
 	db 34, GROWLITHE
 	db 32, PONYTA
-	db 30, GRIMER
-	db 28, PONYTA
-	db 37, WEEZING
-	db 39, MUK
-ENDC
-IF DEF(_BLUE)
-	db 32, GRIMER
-	db 30, GRIMER
-	db 34, PONYTA
-	db 30, PONYTA
-	db 34, VULPIX
-	db 32, PONYTA
-	db 30, KOFFING
-	db 28, PONYTA
-	db 37, MUK
-	db 39, WEEZING
-ENDC
+	db 38, MUK
+	db 38, WEEZING
+	db 31, PONYTA
+	db 33, PONYTA
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
--- data/wild/maps/Route2.asm
+++ data/wild/maps/Route2.asm
@@ -5,20 +5,11 @@ Route2WildMons:
 	db  4, PIDGEY
 	db  4, RATTATA
 	db  5, PIDGEY
-IF DEF(_RED)
-	db  3, WEEDLE
 	db  2, RATTATA
-	db  5, RATTATA
 	db  4, WEEDLE
-	db  5, WEEDLE
-ENDC
-IF DEF(_BLUE)
-	db  3, CATERPIE
-	db  2, RATTATA
-	db  5, RATTATA
 	db  4, CATERPIE
-	db  5, CATERPIE
-ENDC
+	db  3, RATTATA
+	db  5, RATTATA
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
